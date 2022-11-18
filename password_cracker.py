639ef54383950103ff203c4b1bf285e5ba2f87fd
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2022-11-18 22:43:57 +0000

    passing all tests

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,25 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+def getFilelines(file_path):
+  with open(file_path, 'r') as file:
+    lines = file.read().splitlines()
+    return lines
+
+def createDigest(decoded_text):
+  return hashlib.sha1(decoded_text.encode()).hexdigest()
+
+def crack_sha1_hash(hash, use_salts=False):
+  passwords = getFilelines('./top-10000-passwords.txt')
+  for password in passwords:
+    if use_salts:
+      salts = getFilelines('./known-salts.txt')
+      for salt in salts:
+        test_hash_pre = createDigest(f'{salt}{password}')
+        test_hash_app = createDigest(f'{password}{salt}')   
+        if hash == test_hash_pre or hash == test_hash_app:
+          return password
+    else:
+      if hash == createDigest(password):
+        return password
+
+  return 'PASSWORD NOT IN DATABASE'
